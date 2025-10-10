88a2fbf02ed8f7020f540fae0c0d8142a3d7a9e1
sonyeowoon <readyeowoon@gmail.com>
2025-10-11 08:35:19 +0900

    picoshell.c cmplit

--- ex04/picoshell/picoshell.c
+++ ex04/picoshell/picoshell.c
@@ -2,13 +2,52 @@ int	picoshell(char **cmds[])
 {
 	int	fd[2];
 	pid_t	pid;
+	int	prev_fd;
+	int	i;
 
-	pipe(fd);
+	i = 0;
+	prev_fd = -1;
 	while (cmds[i])
 	{
+		if (cmds[i + 1] && pipe(fd))
+			return (1);
 		pid = fork();
 		if (pid == -1)
+		{
+			if (cmds[i+1])
+			{
+				close(fd[0]);
+				close(fd[1]);
+			}
 			return (1);
+		}
 		if (pid == 0)
+		{
+			if (prev_fd != -1)
+			{
+				if (dup2(prev_fd, STDIN_FILENO) == -1)
+					exit(1);
+				close(prev_fd);
+			}
+			if (cmds[i + 1])
+			{
+				close(fd[0]);
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+					exit(1);
+				close(fd[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		if (prev_fd != -1)
+			close(prev_fd);
+		if (cmds[i + 1])
+		{
+			close(fd[1]);
+			prev_fd = fd[0];
+		}
+		i++;
 	}
+	while (wait(NULL) > 0);
+	return (0);
 }
